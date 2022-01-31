5983f84cdfed397a6140d94efd4ef457648868f2
juniorlgx <junior@learnregex.xyz>
2022-01-31 09:56:11 +0700

    add new secret

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 discord_client_secret = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ"
 facebook_access_key = '12345678123456781234567812345678'
 
+SECRET_KEY = 'rzkhwa4n=uh2bd&f6p4@-sz8y4@ytq%*rig%lcmmb6lu&p_$zv'
+
 integration_secret = 'keytoken1234'
 key_map_provider = 'keytoken1234'
 password = 'admin123456789'
